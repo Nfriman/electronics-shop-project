8fb9d890eb002c86c267c7619e79a3edeed72f7a
admi <nariman.ramazaov@yandex.ru>
2023-05-05 00:17:07 +0300

    home work 1 ready +

--- tests/test_item.py
+++ tests/test_item.py
@@ -2,5 +2,4 @@ from src.item import *
 
 
 def test_item():
-    assert Item("Смартфон", 10000, 20).apply_discount() == 10000
     assert Item("Смартфон", 10000, 20).calculate_total_price() == 200000
