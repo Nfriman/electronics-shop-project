7afef6a34668efc552f7dcfddd142cb11c31aaaa
admi <nariman.ramazaov@yandex.ru>
2023-05-09 03:50:52 +0300

    home work 2 ready

--- src/item.py
+++ src/item.py
@@ -1,36 +1,60 @@
+import csv
+import os
+
+
 class Item:
-    """
-    Класс для представления товара в магазине.
-    """
+    """Класс для представления товара в магазине."""
     pay_rate = 1.0
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
-        """
-        Создание экземпляра класса item.
-
+        """Создание экземпляра класса item.
         :param name: Название товара.
         :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
-        """
+        :param quantity: Количество товара в магазине."""
         self.pay = None
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
-        Item.all.append(self)
+        self.all.append(self)
+
+    @property
+    def name(self):
+        ''' Геттер приватнного атрибута name'''
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        """ сеттер name проверяет, что длина наименования товара не больше 10 симвовов"""
+        if len(name) > 10:
+            raise  Exception("Длина наименования товара превышает 10 символов.")
+        self.__name = name
 
     def calculate_total_price(self) -> float:
-        """
-        Рассчитывает общую стоимость конкретного товара в магазине.
-
-        :return: Общая стоимость товара.
-        """
+        """Рассчитывает общую стоимость конкретного товара в магазине.:return: Общая стоимость товара."""
         self.all_price = self.price * self.quantity
         return self.all_price
 
     def apply_discount(self):
-        """
-        Применяет установленную скидку для конкретного товара.
-        """
+        """Применяет установленную скидку для конкретного товара."""
         self.price = int(self.pay_rate*self.price)
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """класс-метод, инициализирующий экземпляры класса `Item` данными из файла _src/items.csv_"""
+        os.chdir('..')
+        file_csv = os.path.join("src", "items.csv")
+        with open(file_csv, newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                cls.all.append(row)
+
+    @staticmethod
+    def string_to_number(number):
+        """статический метод, возвращающий число из числа-строки"""
+        return int(float(number))
+
+
+
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -2,4 +2,22 @@ from src.item import *
 
 
 def test_item():
-    assert Item("Смартфон", 10000, 20).calculate_total_price() == 200000
+    item1 = Item("Смартфон", 10000, 20)
+    assert item1.calculate_total_price() == 200000
+
+    item1.name = 'комп'
+    assert item1.name == 'комп'
+
+
+    item2 = Item.all[0]
+    assert item2.name == 'комп'
+
+    assert Item.string_to_number('5') == 5
+
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 6
+
+    try:
+        item1.name = "!!!!!!!!!!!!!!!!!"
+    except Exception as e:
+        assert str(e) == "Длина наименования товара превышает 10 символов."
